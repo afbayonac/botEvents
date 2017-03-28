589d1bed89b5164550a6da161aef4f74cf653683
afbayonac <afbayonac@gmail.com>
2017-03-28 01:56:18 -0500

    :construction: Add route events/ POST

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express')
+var events = require('../controllers/events')
 var router = express.Router()
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.json({res: 'server express is runnig'})
 })
+
+router.post('/events', events.create)
 
 module.exports = router
